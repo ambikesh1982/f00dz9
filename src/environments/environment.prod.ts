1b86d978e557745a821c6b1876a7bcc5aef99c75
ambikesh1982 <ambikesh1982@gmail.com>
2018-06-21 15:10:10 +0800

    hosted to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBN2Z4XVfAAwMVP_ox6rTR5I7mS4C7yyk0',
+    authDomain: 'f00dz9-app.firebaseapp.com',
+    databaseURL: 'https://f00dz9-app.firebaseio.com',
+    projectId: 'f00dz9-app',
+    storageBucket: 'f00dz9-app.appspot.com',
+    messagingSenderId: '625229359791'
+  }
 };
