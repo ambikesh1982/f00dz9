0ba9861d67a1ad3dbfbfc7c47bafeef92fda9fec
ambikesh1982 <ambikesh1982@gmail.com>
2018-07-11 00:37:48 +0800

    order state change works

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CheckoutService } from './checkout.service';
 import { AuthService } from '../core/auth.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-checkout',
@@ -14,13 +15,45 @@ export class CheckoutComponent implements OnInit {
   currentUser: any;
   // orderStates = ['Confirmed', 'Rejected', 'Partially Accepted'];
 
-  constructor( private checkoutService: CheckoutService, private auth: AuthService) {
+  constructor(private checkoutService: CheckoutService, private auth: AuthService) {
     this.currentUser = this.auth.currAppUser.uid;
-   }
+  }
 
   ngOnInit() {
-    this.checkedoutOrders$ = this.checkoutService.getCheckedOutOrders(this.currentUser);
-    this.receivedOrders$ = this.checkoutService.getMyOrders(this.currentUser);
+    this.checkedoutOrders$ = this.checkoutService.ordersPlaced(this.currentUser)
+      .pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+        ));
+
+    this.receivedOrders$ = this.checkoutService.ordersReceived(this.currentUser)
+      .pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+        ));
+  }
+
+  deleteOrder(id: string) {
+    console.log('TODO: Order cancelled by buyer. Delete order data: ', id);
+    this.checkoutService.deletePlacedOrder(id);
+  }
+
+  cancelOrder(id: string) {
+    this.checkoutService.updateReceivedOrderState(id, 'Cancelled');
+  }
+
+  confirmOrder(id: string) {
+    this.checkoutService.updateReceivedOrderState(id, 'Confirmed');
+  }
+
+  partiallyConfirmOrder(id: string) {
+    this.checkoutService.updateReceivedOrderState(id, 'Partially confirmed');
   }
 
 }
--- src/app/checkout/checkout.service.ts
+++ src/app/checkout/checkout.service.ts
@@ -9,12 +9,29 @@ export class CheckoutService {
 
   constructor(private afs: AngularFirestore, private auth: AuthService) { }
 
-  getCheckedOutOrders(userID: string) {
-    return this.afs.collection('checkout', ref => ref.where('cartID', '==', userID)).valueChanges();
+  ordersPlaced(id: string) {
+    return this.afs.collection('checkout', ref => ref.where('cartID', '==', id)).snapshotChanges();
   }
 
-  getMyOrders(userID: string) {
-    return this.afs.collection('checkout', ref => ref.where('orderID', '==', userID)).valueChanges();
+  ordersReceived(id: string) {
+    return this.afs.collection('checkout', ref => ref.where('orderID', '==', id)).snapshotChanges();
+  }
+
+  deletePlacedOrder(id: string) {
+    this.afs.collection('checkout').doc(id).delete().then(
+      resp => {
+        console.log('Order removed from checkout: ', resp);
+      }
+    ).catch( e => console.log('Error while removing order from checkout: ', e));
+  }
+
+  updateReceivedOrderState(id: string, newState: string) {
+    this.afs.collection('checkout').doc(id).update({state: newState})
+      .then(
+        resp => {
+          console.log('Order status updated: ', resp);
+        }
+      ).catch(e => console.log('Error while updating order status: ', e));
   }
 
 }
